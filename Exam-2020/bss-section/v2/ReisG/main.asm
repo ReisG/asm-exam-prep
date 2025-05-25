3a3b81f5f6484111a54c229692e2ceed52455234
ReisG <romanryabcev2006@gmail.com>
2025-05-25 17:12:53 +0300

    created solution for v2

---
+++ Exam-2020/bss-section/v2/ReisG/main.asm
@@ -0,0 +1,15 @@
+section .data
+    wow dw 0x1234, 0xbeaf
+    tasty dd 0x00102030
+
+section .bss
+    beaf resd 1
+
+section .text
+global main
+main:
+    movsx eax, byte[wow + 3]
+    sub ax, 0x4321
+    xchg ah, byte[tasty + 1]
+    xor eax, eax
+    ret
